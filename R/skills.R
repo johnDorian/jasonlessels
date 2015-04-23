b4e498683a17825c33ce8f562bc1fb6a54f71792
johnDorian <jlessels@gmail.com>
2015-04-23 19:04:11 +0100

    added skills function

---
+++ R/skills.R
@@ -0,0 +1,17 @@
+#'
+#'Skills
+#'
+#'This function lists my skills in markdown format.
+#'
+#'@export
+Skills <- function(){
+  cat("##Computing skills:")
+  cat("\n\n")
+  cat("* Applications: MS Office, ArcGIS, GRASS, QGIS, Latex, Genstat, JMP\n")
+  cat("* Programming Languages: R, C++, and experience with: Python, Java, Fortran, Matlab")
+  cat("##Teaching skills:")
+  cat("\n\n")
+  cat("* Supervised first, second and third year applied statistics practicals and exams.")
+  cat("* Have lead first, second and third year applied statistics tutorials.")
+  cat("* Supervised second year hydrology practicals.")
+}
