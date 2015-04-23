3aa23fcf3338cf56935f5d35cf224f5932f3a42a
johnDorian <jlessels@gmail.com>
2015-04-23 19:33:51 +0100

    Add the packages function

---
+++ R/R_packages.R
@@ -0,0 +1,13 @@
+#'
+#'R packages
+#'
+#'This function lists my education in markdown format.
+#'
+#'@export
+R_packages <- function(){
+  cat("**TSAgg:** Time series aggregation for incomplete time series data.\n")
+  cat("**BomDataRipper:** A package that provides the ability to download weather data from the Australian Bureau of Meteorology website.\n")
+  cat("**WIDataRipper:** This package provides the ability to obtain data from the NSW govern- ment water information website from within R.\n")
+  cat("**hydroEFS:** Implemented in R and C++, this package provides the ability to find and sum- marise streamflow events.\n")
+  cat("**geoRExtended:** This package provides additional optimising methods for the likelihood function and also provides additional functions.\n")
+}
