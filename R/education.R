50333d23345e9085483e1439b0293eb63cfb186a
johnDorian <jlessels@gmail.com>
2015-04-23 18:44:41 +0100

    Added education function and documentation

---
+++ R/education.R
@@ -0,0 +1,23 @@
+#'
+#'Education
+#'
+#'This function lists my education in markdown format.
+#'
+#'@export
+#'
+Education <- function(){
+  cat("**2014, Machine Learning, Coursea (Stanford University)**")
+  cat("\n\n")
+  cat("I performed race contestant profiling and post race analysis")
+  cat("\n\n")
+  cat("**2009 - 2013, PhD, University of Sydney**")
+  cat("\n\n")
+  cat("Thesis Title: The optimisation of water quality sampling and load estimation\n")
+  cat("Modules included: Introduction to Programming (High Distinction), Life Sciences Calculus\n")
+  cat("\n\n")
+  cat("**2004 - 2008, Bachelor Land and Water Science (Hons), University of Sydney**")
+  cat("\n\n")
+  cat("First Class Honours, Deans award\n")
+  cat("Modules included: statistics, hydrology, soil science, GIS remote sensing, chemistry, geology, agronomy\n")
+
+}
