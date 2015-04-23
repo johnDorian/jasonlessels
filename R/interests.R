c20707c81c7e4875cdbc748d9fab5310ef0d6659
johnDorian <jlessels@gmail.com>
2015-04-23 21:46:14 +0100

    added interests function

---
+++ R/interests.R
@@ -0,0 +1,14 @@
+#'
+#'Interests and Hobbies
+#'
+#'This function lists my interests and hobbiesin markdown format.
+#'
+#'@export
+Interests_and_hobbies<- function(){
+  cat("^Electronics:^ I like applying my coding knowledge with electronics. I have built several sensors which I have used to strengthen my research")
+  cat("\n\n")
+  cat("^Hillwalking:^ I like the challenge of completing long hikeshikes, e. g. in New Zealand, Norway and Australia")
+  cat("\n\n")
+  cat("^Mountain biking:^ I enjoy mountain biking. I have ridden in Scotland, Switzerland and Australia")
+  cat("\n\n")
+}
