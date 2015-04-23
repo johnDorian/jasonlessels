c05811d3aafd93dea0e0a97cbf11731d04ba6f14
johnDorian <jlessels@gmail.com>
2015-04-23 19:03:48 +0100

    Tidied up functions

--- R/education.R
+++ R/education.R
@@ -4,20 +4,18 @@
 #'This function lists my education in markdown format.
 #'
 #'@export
-#'
 Education <- function(){
   cat("**2014, Machine Learning, Coursea (Stanford University)**")
-  cat("\n\n")
-  cat("I performed race contestant profiling and post race analysis")
   cat("\n\n")
   cat("**2009 - 2013, PhD, University of Sydney**")
   cat("\n\n")
   cat("Thesis Title: The optimisation of water quality sampling and load estimation\n")
-  cat("Modules included: Introduction to Programming (High Distinction), Life Sciences Calculus\n")
+  cat("\n")
+  cat("Modules included: Introduction to Programming (High Distinction), Life Sciences Calculus")
   cat("\n\n")
   cat("**2004 - 2008, Bachelor Land and Water Science (Hons), University of Sydney**")
   cat("\n\n")
-  cat("First Class Honours, Deans award\n")
+  cat("First Class Honours, Deans award")
+  cat("\n")
   cat("Modules included: statistics, hydrology, soil science, GIS remote sensing, chemistry, geology, agronomy\n")
-
 }
--- R/employment.R
+++ R/employment.R
@@ -12,7 +12,7 @@ Employment <- function(){
   cat("\n\n")
   cat("**2013 - Present, Post Doctoral Research Fellow, Aberdeen University**")
   cat("\n\n")
-  cat("Responsible for integrating hydrological and ecosystem models to understand the implica- tions of climate change on soil carbon dynamics.")
+  cat("Responsible for integrating hydrological and ecosystem models to understand the implications of climate change on soil carbon dynamics.")
   cat("\n\n")
   cat("**2008 - 2013, Environmental Statistics Tutor.**")
   cat("\n\n")
