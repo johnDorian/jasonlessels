a0573c9a265b6d893e525e5f0eb01014c8e6e05c
johnDorian <jlessels@gmail.com>
2015-04-23 20:34:28 +0100

    changes to the papers function

---
+++ R/papers.R
@@ -0,0 +1,18 @@
+#'
+#'Refereed Papers
+#'
+#'This function lists my papers in markdown format.
+#'
+#'@export
+Refereed_papers <- function(){
+  cat("Karunaratne, SB, Bishop, TFA, Lessels, JS, Baldock, JA and Odeh, IOA (2014) **A space-time observation system for soil organic carbon**, Soil Research, accepted, in press")
+  cat("\n\n")
+  cat("Lessels, JS and Bishop, TFA (2013) **Estimating water quality using linear mixed models with stream discharge and turbidity**, Journal of Hydrology, (498) 13-22")
+  cat("\n\n")
+  cat("Lessels, JS and Bishop, TFA (2013) **Estimating the effect of sample size on the uncertainty of mean total phosphorus using historical data**, Hydrological Processes, Accepted, in press")
+  cat("\n\n")
+  cat("Lessels, JS and Bishop, TFA (2012) **Event-based water quality monitoring using strat- ified random sampling designs**, In Proceedings Hydrology and Water Resources Symposium.")
+  cat("\n\n")
+  cat("Lessels, JS and Bishop, TFA (2011), **A geostatistical comparison between routine and event-based water quality sampling**, In Proceedings of the 34th World Congress of the International Association for Hydro-Environment Research and Engineering: 33rd Hydrology and Water Resources Symposium and 10th Conference on Hydraulics in Water Engineering.")
+  cat("\n\n")
+}
