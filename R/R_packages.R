666d93a996c9427692352896e221b814bdbf1eee
johnDorian <jlessels@gmail.com>
2015-04-23 20:30:03 +0100

    changes to functions.

--- R/R_packages.R
+++ R/R_packages.R
@@ -1,13 +1,20 @@
 #'
 #'R packages
 #'
-#'This function lists my education in markdown format.
+#'This function lists my R packages in markdown format.
 #'
 #'@export
 R_packages <- function(){
-  cat("**TSAgg:** Time series aggregation for incomplete time series data.\n")
-  cat("**BomDataRipper:** A package that provides the ability to download weather data from the Australian Bureau of Meteorology website.\n")
-  cat("**WIDataRipper:** This package provides the ability to obtain data from the NSW govern- ment water information website from within R.\n")
-  cat("**hydroEFS:** Implemented in R and C++, this package provides the ability to find and sum- marise streamflow events.\n")
-  cat("**geoRExtended:** This package provides additional optimising methods for the likelihood function and also provides additional functions.\n")
+  cat("[**ggsnippets:**](https://github.com/johnDorian/ggsnippets) Additional functions for the `ggplot2` package.")
+  cat("\n\n")
+  cat("[**TSAgg:**](https://github.com/johnDorian/TSAgg) Time series aggregation for incomplete time series data.")
+  cat("\n\n")
+  cat("[**BomDataRipper:**](https://github.com/johnDorian/BOMdataRipper) A package that provides the ability to download weather data from the Australian Bureau of Meteorology website.")
+  cat("\n\n")
+  cat("[**WIDataRipper:**](https://r-forge.r-project.org/R/?group_id=1205) This package provides the ability to obtain data from the NSW government water information website from within R.")
+  cat("\n\n")
+  cat("[**hydroEFS:**](https://r-forge.r-project.org/R/?group_id=1205) Implemented in R and C++, this package provides the ability to find and summarise streamflow events.")
+  cat("\n\n")
+  cat("[**geoRExtended:**](https://r-forge.r-project.org/R/?group_id=1205) This package provides additional optimising methods for the likelihood function and also provides additional functions.")
+  cat("\n\n")
 }
--- R/employment.R
+++ R/employment.R
@@ -14,7 +14,7 @@ Employment <- function(){
   cat("\n\n")
   cat("Responsible for integrating hydrological and ecosystem models to understand the implications of climate change on soil carbon dynamics.")
   cat("\n\n")
-  cat("### 2008 - 2013, Environmental Statistics Tutor.")
+  cat("### 2008 - 2013, Environmental Statistics Tutor")
   cat("\n\n")
   cat("Tutored classes in statistical theory and lead practical classes using R and other statistical software.")
   cat("\n\n")
