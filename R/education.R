557d4d31a42356a8ca6559d22089d7e51c021237
johnDorian <jlessels@gmail.com>
2015-04-24 12:11:36 +0100

    Tidying up

--- R/education.R
+++ R/education.R
@@ -5,11 +5,11 @@
 #'
 #'@export
 Education <- function(){
-  cat("### 2014, Machine Learning, Coursea (Stanford University)")
+  cat("**2014, Machine Learning, Coursea (Stanford University)**")
   cat("\n\n")
-  cat("### 2009 - 2013, PhD, University of Sydney")
+  cat("**2009 - 2013, PhD, University of Sydney**")
   cat("\n\n")
   cat("Thesis Title: The optimisation of water quality sampling and load estimation")
   cat("\n\n")
-  cat("### 2004 - 2008, Bachelor Land and Water Science (First Class Hons), University of Sydney")
+  cat("**2004 - 2008, Bachelor Land and Water Science (First Class Hons), University of Sydney**")
 }
--- R/employment.R
+++ R/employment.R
@@ -6,15 +6,15 @@
 #'@export
 #'
 Employment <- function(){
-  cat("### 2014 - Present, Data Analyst, Firetrail Events")
+  cat("**2014 - Present, Data Analyst, Firetrail Events**")
   cat("\n\n")
   cat("Perform data management, sales reporting, and competitor analysis.")
   cat("\n\n")
-  cat("### 2013 - Present, Post Doctoral Research Fellow, University of Aberdeen")
+  cat("**2013 - Present, Post Doctoral Research Fellow, University of Aberdeen**")
   cat("\n\n")
   cat("Responsible for integrating hydrological and ecosystem models to examine the implications of climate change on soil carbon dynamics in Arctic environments.")
   cat("\n\n")
-  cat("### 2008 - 2013, Environmental Statistics Tutor")
+  cat("**2008 - 2013, Environmental Statistics Tutor**")
   cat("\n\n")
   cat("Tutored 1st, 2nd and 3rd year classes in statistical theory and lead practical classes using R and other statistical packages.")
 }
