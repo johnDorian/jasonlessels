7072abb59f457ce8f34a826b7e4f47ae2277ce21
johnDorian <jlessels@gmail.com>
2015-04-24 12:19:05 +0100

    minor changes

--- R/interests.R
+++ R/interests.R
@@ -7,7 +7,7 @@
 Interests_and_hobbies <- function(){
   cat("*Electronics:* I like applying my coding knowledge with electronics. I have built several sensors which I have used to for my scientific research")
   cat("\n\n")
-  cat("*Hillwalking:* I like the challenge of completing long hikes, e. g. in New Zealand, Norway and Australia")
+  cat("*Hillwalking:* I like the challenge of completing long hikes, e.g. in New Zealand, Norway and Australia")
   cat("\n\n")
   cat("*Mountain biking:* I enjoy mountain biking. I have ridden in Scotland, Switzerland and Australia")
 }
--- R/papers.R
+++ R/papers.R
@@ -5,13 +5,13 @@
 #'
 #'@export
 Selected_papers_and_conferences <- function(){
-  cat("Karunaratne, SB, Bishop, TFA, Lessels, JS, Baldock, JA and Odeh, IOA (2014) **A space-time observation system for soil organic carbon**, Soil Research, accepted, in press")
+  cat("Karunaratne, SB, Bishop, TFA, Lessels, JS, Baldock, JA and Odeh, IOA (2015) **A space-time observation system for soil organic carbon**, Soil Research, accepted, in press")
   cat("\n\n")
-  cat("Lessels, JS and Bishop, TFA (2013) **Estimating water quality using linear mixed models with stream discharge and turbidity**, Journal of Hydrology, (498) 13-22")
+  cat("**Using isotopes to investigate hydrological flow pathways and sources in a remote Arctic catchment**, presented at the European Geophysical Union, General Assembly,  2014")
   cat("\n\n")
   cat("Lessels, JS and Bishop, TFA (2013) **Estimating the effect of sample size on the uncertainty of mean total phosphorus using historical data**, Hydrological Processes, Accepted, in press")
   cat("\n\n")
-  cat("**Using isotopes to investigate hydrological flow pathways and sources in a remote Arctic catchment**, presented at the European Geophysical Union, General Assembly,  2014")
+  cat("Lessels, JS and Bishop, TFA (2013) **Estimating water quality using linear mixed models with stream discharge and turbidity**, Journal of Hydrology, (498) 13-22")
   cat("\n\n")
   cat("**Applying geospatial techniques to temporal data**, presented at useR!,  2011")
   cat("\n\n")
